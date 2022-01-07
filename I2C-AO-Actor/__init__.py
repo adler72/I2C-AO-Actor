b5e02e1e4a4448e14ec375f5130a80dbe633b405
adler72 <53665786+adler72@users.noreply.github.com>
2022-01-07 14:26:20 +0100

    Update __init__.py

--- I2C-AO-Actor/__init__.py
+++ I2C-AO-Actor/__init__.py
@@ -26,59 +26,60 @@ def get_Address():
 
 @parameters([Property.Select("Address AO", get_Address(), description="The I2C actor address."),Property.Select("Port AO", options=[0x00,0x01,0x02,0x03], description="Port of the Analog-Ouput-Modul to which the actor is connected")])
 class CustomActor(CBPiActor):
-     @action("Set Power", parameters=[Property.Number(label="Power", configurable=True,description="Power Setting [0-100]")])
-    async def setpower(self,Power = 100 ,**kwargs):
-        self.power=int(Power)
-        if self.power < 0:
-            self.power = 0
-        if self.power > 100:
-            self.power = 100           
-        await self.set_power(self.power)      
+   @action("Set Power", parameters=[Property.Number(label="Power", configurable=True,description="Power Setting [0-100]")])
+
+   async def setpower(self,Power = 100 ,**kwargs):
+      self.power=int(Power)
+      if self.power < 0:
+          self.power = 0
+      if self.power > 100:
+          self.power = 100           
+      await self.set_power(self.power)      
      
-    def init(self, props):
-        self.state = False
-        self.adress_AO = int(self.props.get("Adress AO",88))
-        self.port_AO = int(self.props.get("Port AO",0))
-        self.bus = SMBus(1) # 1 indicates /dev/i2c-1
-        pass
+   def init(self, props):
+      self.state = False
+      self.adress_AO = int(self.props.get("Adress AO",88))
+      self.port_AO = int(self.props.get("Port AO",0))
+      self.bus = SMBus(1) # 1 indicates /dev/i2c-1
+      pass
 
-    async def on(self, power=0):
-        logger.info("ACTOR %s ON" % self.id)
-        self.power = int(power)
-        HBy = int(int(self.power)*10.23/256)
-        LBy = int(int(self.power)*10.23-HBy*256)
-        field=[LBy,HBy]
-        try:
-            self.bus.write_i2c_block_data(int(self.address_AO),int(self.port_AO,16),field)
-        except: # exception if write_byte fails
-            pass  
-        self.state = True
+   async def on(self, power=0):
+      logger.info("ACTOR %s ON" % self.id)
+      self.power = int(power)
+      HBy = int(int(self.power)*10.23/256)
+      LBy = int(int(self.power)*10.23-HBy*256)
+      field=[LBy,HBy]
+      try:
+          self.bus.write_i2c_block_data(int(self.address_AO),int(self.port_AO,16),field)
+      except: # exception if write_byte fails
+          pass  
+      self.state = True
 
 
-    async def off(self):
-        logger.info("ACTOR %s OFF " % self.id)
-        HBy = 0
-        LBy = 0
-        field=[LBy,HBy]
-        try:
-            self.bus.write_i2c_block_data(int(self.address_AO),int(self.port_AO,16),field) 
-        except: # exception if write_byte fails
-            pass
-        self.state = False
+   async def off(self):
+      logger.info("ACTOR %s OFF " % self.id)
+      HBy = 0
+      LBy = 0
+      field=[LBy,HBy]
+      try:
+          self.bus.write_i2c_block_data(int(self.address_AO),int(self.port_AO,16),field) 
+      except: # exception if write_byte fails
+          pass
+      self.state = False
     
-    async def set_power(self, power):
-        self.power = power
-        await self.cbpi.actor.actor_update(self.id,power)
-        pass
+   async def set_power(self, power):
+      self.power = power
+      await self.cbpi.actor.actor_update(self.id,power)
+      pass
    
 
-    def get_state(self):
-        return self.state
+   def get_state(self):
+      return self.state
     
-    async def run(self):
-        pass
+   async def run(self):
+      pass
 
 
-def setup(cbpi):
-    cbpi.plugin.register("I2C-AO-Actor", CustomActor)
-    pass
+   def setup(cbpi):
+      cbpi.plugin.register("I2C-AO-Actor", CustomActor)
+      pass
