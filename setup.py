ff32b90aeb9a51e49f4244947c673a40177a44b7
adler72 <53665786+adler72@users.noreply.github.com>
2022-01-07 10:20:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='I2C-AO-Actor',
-      version='0.0.19',
+      version='0.0.20',
       description='CraftBeerPi Plugin',
       author='Marc Adler',
       author_email='aeda@gmx.de',
