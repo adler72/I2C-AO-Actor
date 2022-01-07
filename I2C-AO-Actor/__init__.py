93218406317382716a273d7747ea17577e7ff630
adler72 <53665786+adler72@users.noreply.github.com>
2022-01-07 13:59:55 +0100

    Update __init__.py

--- I2C-AO-Actor/__init__.py
+++ I2C-AO-Actor/__init__.py
@@ -87,21 +87,7 @@ class CustomActor(CBPiActor):
     def get_state(self):
         return self.state
     
-    async def run(self,power):
-       
-        while self.running == True:
-           self.power = power
-           HBy = int(int(self.power)*10.23/256)
-           LBy = int(int(self.power)*10.23-HBy*256)
-           field=[LBy,HBy]
-           bus = SMBus(1) # 1 indicates /dev/i2c-1
-           try:
-              bus.write_i2c_block_data(0x58,0x00,field) 
-           except: # exception if write_byte fails
-            pass  
-        await self.cbpi.actor.actor_update(self.id,power)
-        pass
-
+    
    def setup(cbpi):
     cbpi.plugin.register("I2C-AO-Actor", CustomActor)
     pass
